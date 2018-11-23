7798a20c6ab2c7bc28d49674d90b49b97f96bdd2
¨Gus <agustincedeno87@gmail.com>
2018-11-22 22:53:16 -0600

    Copia de proyecto

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -3,7 +3,6 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 const pagesRoutes: Routes = [
   {
@@ -13,8 +12,6 @@ const pagesRoutes: Routes = [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'progress', component: ProgressComponent },
       { path: 'graficas', component: Graficas1Component },
-      { path: 'account-settings', component: AccountSettingsComponent },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
     ]
   }
 ];
